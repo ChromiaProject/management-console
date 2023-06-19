1c8475fab139313f57d5d34aa062642e0b4db3b2
Mikael Ståldal <mikael.staldal@chromaway.com>
2023-06-19 13:32:36 +0000

    POS-783: Include signers when calculating blockchain RID

--- src/main/kotlin/net/postchain/mc/cli/blockchain/CommandProposeBlockchain.kt
+++ src/main/kotlin/net/postchain/mc/cli/blockchain/CommandProposeBlockchain.kt
@@ -1,11 +1,18 @@
 package net.postchain.mc.cli.blockchain
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.CliktError
+import com.github.ajalt.clikt.core.PrintMessage
+import com.github.ajalt.clikt.core.ProgramResult
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import net.postchain.chain0.proposal_blockchain.findBlockchainRid
 import net.postchain.chain0.proposal_blockchain.proposeBlockchainOperation
-import net.postchain.mc.cli.base.printResult
+import net.postchain.chain0.version.apiVersion
+import net.postchain.common.hexStringToByteArray
+import net.postchain.common.toHex
+import net.postchain.common.tx.TransactionStatus
 import net.postchain.mc.cli.base.pubkey
 import net.postchain.mc.cli.blockchainConfigOption
 import net.postchain.mc.cli.util.BlockchainConfig
@@ -25,24 +32,38 @@ class CommandProposeBlockchain : CliktCommand(
 
     private val name by nameOption("Name of blockchain").required()
 
-    private val quiet by option("-q", "--quiet", help = "Only prints Blockchain RID if succeeds").flag()
+    private val quiet by option("-q", "--quiet", help = "Print only blockchain RID if succeeds").flag()
 
     private val description by proposalDescriptionOption()
 
     override fun run() {
+        val apiVersion = client.apiVersion()
         val bcConfig = BlockchainConfig.readFromFile(blockchainConfigFile)
-        client.transactionBuilder()
+        val txResult = client.transactionBuilder()
                 .proposeBlockchainOperation(client.pubkey, bcConfig.data, name, container, description)
                 .postAwaitConfirmation()
                 .apply {
-                    if (quiet) {
-                        printResult(bcConfig.hash.toString(), "")
-                    } else {
-                        printResult(
-                                "Blockchain $name has been proposed: ${bcConfig.hash}",
-                                "Cannot add bc proposal"
-                        )
+                    when (status) {
+                        TransactionStatus.CONFIRMED -> {}
+                        TransactionStatus.REJECTED -> throw CliktError("Cannot add bc proposal: $rejectReason")
+                        TransactionStatus.WAITING -> if (quiet) {
+                            echo("Transaction not confirmed", err = true)
+                            throw ProgramResult(0)
+                        } else
+                            throw PrintMessage("Transaction not confirmed")
+                        else -> throw CliktError("Cannot find status for this transaction")
                     }
                 }
+
+        if (apiVersion >= 8) {
+            val maybeBcRid: ByteArray? = client.findBlockchainRid(txResult.txRid.rid.hexStringToByteArray())
+            if (maybeBcRid != null) {
+                echo(if (quiet) maybeBcRid.toHex() else "Blockchain $name has been added, bc-rid: $maybeBcRid.toHex()}")
+            } else {
+                echo("Blockchain $name has been proposed, tx-rid: ${txResult.txRid.rid}", err = quiet)
+            }
+        } else {
+            echo(if (quiet) bcConfig.hash.toHex() else "Blockchain $name has been proposed, bc-rid: ${bcConfig.hash.toHex()}")
+        }
     }
 }
