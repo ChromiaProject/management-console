c1fca19ab67262a0261a0a3d2e4610159135e1d5
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-07-21 12:28:59 +0300

    PMC CLI: add `dilithium` key support to the `keygen` command

--- src/main/kotlin/net/postchain/mc/cli/keys/CommandKeygen.kt
+++ src/main/kotlin/net/postchain/mc/cli/keys/CommandKeygen.kt
@@ -1,20 +1,31 @@
 package net.postchain.mc.cli.keys
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.groups.default
+import com.github.ajalt.clikt.parameters.groups.mutuallyExclusiveOptions
+import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
 import net.postchain.common.toHex
+import net.postchain.crypto.DilithiumCryptoSystem
 import net.postchain.crypto.KeyPair
 import net.postchain.crypto.PrivKey
 import net.postchain.crypto.PubKey
 import net.postchain.crypto.Secp256K1CryptoSystem
 import net.postchain.crypto.secp256k1_derivePubKey
+import net.postchain.mc.cli.keys.CryptoSystemType.DILITHIUM
+import net.postchain.mc.cli.keys.CryptoSystemType.ECDSA
 import org.bitcoinj.crypto.MnemonicCode
 import java.io.File
 import java.io.FileOutputStream
 import java.util.*
+
+enum class CryptoSystemType(val option: String) {
+    ECDSA("--ecdsa"),
+    DILITHIUM("--dilithium")
+}
 
 class CommandKeygen : CliktCommand(name = "keygen", help = "Generates public/private key pair") {
 
@@ -32,22 +43,40 @@ class CommandKeygen : CliktCommand(name = "keygen", help = "Generates public/pri
 
     private val nodeFormat by option("-n", "--node", help = "Save the generated keypair in format to be included in node properties file").flag()
 
+    private val cs by mutuallyExclusiveOptions(
+            option(ECDSA.option, help = "ECDSA keys").flag().convert { ECDSA },
+            option(DILITHIUM.option, help = "Dilithium keys").flag().convert { DILITHIUM },
+            name = "Provider tier",
+    ).default(ECDSA)
+
     /**
      * Cryptographic key generator. Will generate a pair of public and private keys and print to stdout.
      */
     override fun run() {
-        val (keyPair, mnemonic) = generateSecp256k1KeyPairWithMnemonic(wordList)
+        if (cs == ECDSA) {
+            val (keyPair, mnemonic) = generateSecp256k1KeyPairWithMnemonic(wordList)
 
-        file?.let {
-            saveSecp256k1KeyPair(keyPair, it, nodeFormat)
-        }
-        echo(
-                """
+            file?.let {
+                saveKeyPair(keyPair, it, nodeFormat)
+            }
+            echo(
+                    """
             |privkey:   ${keyPair.privKey.data.toHex()}
             |pubkey:    ${keyPair.pubKey.data.toHex()}
             |mnemonic:  $mnemonic 
         """.trimMargin()
-        )
+            )
+        } else if (cs == DILITHIUM) {
+            if (file == null) {
+                error("--save option must be specified in case of ${DILITHIUM.option}")
+            }
+
+            val keyPair = generateDilithiumKeyPair()
+            file?.let {
+                saveKeyPair(keyPair, it, nodeFormat)
+            }
+            echo(keyPair.pubKey.data.toHex())
+        }
     }
 }
 
@@ -70,7 +99,7 @@ private fun generateSecp256k1KeyPairWithMnemonic(wordList: String): Pair<KeyPair
     return keyPair to mnemonic
 }
 
-private fun saveSecp256k1KeyPair(keyPair: KeyPair, file: File, nodeFormat: Boolean) {
+private fun saveKeyPair(keyPair: KeyPair, file: File, nodeFormat: Boolean) {
     if (file.parentFile != null && !file.parentFile.exists()) file.parentFile.mkdirs()
     val prefix = if (nodeFormat) "messaging." else ""
     val properties = Properties()
@@ -78,7 +107,9 @@ private fun saveSecp256k1KeyPair(keyPair: KeyPair, file: File, nodeFormat: Boole
     properties["${prefix}pubkey"] = keyPair.pubKey.data.toHex()
 
     FileOutputStream(file).use { fs ->
-        properties.store(fs, "Keypair generated using secp256k1")
+        properties.store(fs, "Keypair generated")
         fs.flush()
     }
 }
+
+fun generateDilithiumKeyPair(): KeyPair = DilithiumCryptoSystem().generateKeyPair()
