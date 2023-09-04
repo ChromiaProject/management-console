f898cbd466c8db767053e045235f37e94db23965
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-09-04 13:54:47 +0300

    get-all-configurations: list only mode implemented (w/o downloading and saving)

--- src/main/kotlin/net/postchain/mc/cli/blockchain/CommandGetAllBlockchainConfigurations.kt
+++ src/main/kotlin/net/postchain/mc/cli/blockchain/CommandGetAllBlockchainConfigurations.kt
@@ -6,6 +6,7 @@ import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.file
+import net.postchain.chain0.common.queries.getBlockchains
 import net.postchain.chain0.nm_api.nmFindNextConfigurationHeight
 import net.postchain.chain0.nm_api.nmGetBlockchainConfiguration
 import net.postchain.gtv.GtvDecoder
@@ -17,39 +18,53 @@ import java.io.File
 
 class CommandGetAllBlockchainConfigurations : CliktCommand(
         name = "get-all-configurations",
-        help = "Download all blockchain configurations and save them to the specified directory"
+        help = "List all blockchain configuration heights or download and save all configurations to the specified directory"
 ) {
     private val config by pmcConfigOption()
 
     private val blockchainRID by blockchainRidOption().required()
 
-    private val save by option(help = "Where to save configurations XML").file(canBeFile = false, canBeDir = true).required()
+    private val save by option(help = "Where to save configurations XML").file(canBeFile = false, canBeDir = true)
 
     private val overwrite by option("--overwrite", help = "Overwrite existing files").flag()
 
     override fun run() {
-        val heights = mutableListOf(0L)
-        save.mkdirs()
-
-        if (!overwrite && save.list()?.isNotEmpty() == true) {
-            echo("Directory is not empty: $save", err = true)
+        if (config.client.getBlockchains(true).none { it.rid == blockchainRID.wData }) {
+            echo("Unknown blockchain: $blockchainRID")
             return
         }
 
-        while (true) {
-            val bcConfig = config.client.nmGetBlockchainConfiguration(blockchainRID, heights.last())
-            if (bcConfig == null) {
-                echo("Blockchain configuration at height ${heights.last()} is absent", err = true)
+        val heights = mutableListOf(0L)
+
+        if (save != null) {
+            save?.mkdirs()
+            if (!overwrite && save?.list()?.isNotEmpty() == true) {
+                echo("Directory is not empty: $save", err = true)
                 return
             }
+        }
 
-            val xmlGtv = GtvMLEncoder.encodeXMLGtv(GtvDecoder.decodeGtv(bcConfig))
-            File(save.path, "${heights.last()}.conf.xml").writeText(xmlGtv)
+        while (true) {
+            if (save != null) {
+                val bcConfig = config.client.nmGetBlockchainConfiguration(blockchainRID, heights.last())
+                if (bcConfig == null) {
+                    echo("Blockchain configuration at height ${heights.last()} is absent", err = true)
+                    heights.removeLast()
+                    return
+                }
+
+                val xmlGtv = GtvMLEncoder.encodeXMLGtv(GtvDecoder.decodeGtv(bcConfig))
+                File(save?.path, "${heights.last()}.conf.xml").writeText(xmlGtv)
+            }
 
             val nextHeight = config.client.nmFindNextConfigurationHeight(blockchainRID, heights.last()) ?: break
             heights.add(nextHeight)
         }
 
-        echo("Configurations at heights downloaded: ${heights.joinToString(", ")}")
+        if (heights.isEmpty()) {
+            echo("No configurations ${if (save != null) "downloaded" else "found"}")
+        } else {
+            echo("Configurations at heights ${if (save != null) "downloaded" else "found"}: ${heights.joinToString(", ")}")
+        }
     }
 }
