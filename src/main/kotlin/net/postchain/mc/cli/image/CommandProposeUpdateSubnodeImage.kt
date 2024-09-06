be2aede29cafbef00cc1f947d644769f79c99e5c
Mikael Ståldal <mikael.staldal@chromaway.com>
2024-09-06 14:38:38 +0200

    Improve input validation in subnode image commands

--- src/main/kotlin/net/postchain/mc/cli/image/CommandProposeUpdateSubnodeImage.kt
+++ src/main/kotlin/net/postchain/mc/cli/image/CommandProposeUpdateSubnodeImage.kt
@@ -4,9 +4,12 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.groups.provideDelegate
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.options.validate
 import net.postchain.chain0.proposal_subnode_image.proposeUpdateSubnodeImageOperation
 import net.postchain.mc.cli.base.printResult
 import net.postchain.mc.cli.base.pubkey
+import net.postchain.mc.cli.util.digestValidator
+import net.postchain.mc.cli.util.metadataTextValidator
 import net.postchain.mc.cli.util.nameOption
 import net.postchain.mc.cli.util.pmcConfigOption
 import net.postchain.mc.cli.util.proposalDescriptionOption
@@ -21,8 +24,8 @@ class CommandProposeUpdateSubnodeImage : CliktCommand(
     private val client get() = config.client
 
     private val name by nameOption("Image name").required()
-    private val url by option(help = "Image URL").required()
-    private val digest by option(help = "Image digest").required()
+    private val url by option(help = "Image URL").required().validate(metadataTextValidator())
+    private val digest by option(help = "Image digest").required().validate(digestValidator())
 
     private val description by proposalDescriptionOption { "Update subnode image $name with URL $url and digest $digest" }
 
