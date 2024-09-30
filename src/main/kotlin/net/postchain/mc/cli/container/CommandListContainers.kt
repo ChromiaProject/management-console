befa793df8ee444696067d355906b91da943d181
Johan Nilsson <johan.nilsson@chromaway.com>
2024-09-30 16:58:56 +0200

    Chain configuration delay

--- src/main/kotlin/net/postchain/mc/cli/container/CommandListContainers.kt
+++ src/main/kotlin/net/postchain/mc/cli/container/CommandListContainers.kt
@@ -31,7 +31,7 @@ class CommandListContainers : CliktCommand(
                 client.getBlockchainInfoListV3(false).map {
                     BlockchainInfo(
                             it.rid, it.name, BlockchainState.RUNNING, it.container, it.cluster, null,
-                            null, null, null
+                            null, null, null, null
                     )
                 }.groupBy { it.container }
             }
