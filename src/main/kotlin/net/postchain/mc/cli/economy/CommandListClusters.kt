e48f288022518d6a036482d106524b672f588204
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-01-10 17:46:02 +0100

    POS-1652: return more data when listing clusters in economy chain

--- src/main/kotlin/net/postchain/mc/cli/economy/CommandListClusters.kt
+++ src/main/kotlin/net/postchain/mc/cli/economy/CommandListClusters.kt
@@ -3,6 +3,7 @@ package net.postchain.mc.cli.economy
 import net.postchain.client.core.PostchainClient
 import net.postchain.economy.economy_chain.apiVersion
 import net.postchain.economy.economy_chain.getClusters
+import net.postchain.mc.cli.base.NAME_LENGTH_MAX
 import net.postchain.mc.cli.cluster.showClusterInfo
 import net.postchain.mc.cli.interactiveOption
 import net.postchain.mc.cli.promptForIndex
@@ -15,15 +16,17 @@ class CommandListClusters : ECBaseCommand(
 ) {
     private val interactive by interactiveOption()
 
-    private val headers = listOf("Name", "Tag")
-
     override fun runEC(client: PostchainClient, economyChainClient: PostchainClient) {
 
         val clusters = economyChainClient.getClusters()
         echo(pmcTable(
                 "clusters",
-                headers,
-                clusters.map { listOf(it.name, it.tagName) },
+                listOf("Name", "Tag", "Operational", "Number of nodes", "Cluster Units", "Extra Storage"),
+                clusters.map {
+                    listOf(it.name, it.tagName, it.isOperational.toString(), it.numberOfNodes?.toString() ?: "",
+                            it.clusterUnits?.toString() ?: "", it.extraStorage?.toString() ?: "")
+                },
+                0 to NAME_LENGTH_MAX,
                 interactive = interactive
         ))
         if (interactive && clusters.isNotEmpty()) {
