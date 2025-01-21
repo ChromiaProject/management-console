23e1a97a0581ffa640895844c4713e95461241b9
Johan Nilsson <johan.nilsson@chromaway.com>
2025-01-21 08:23:13 +0100

    Replace config merkle hash code with function in postchain

--- src/main/kotlin/net/postchain/mc/cli/util/blockchainConfig.kt
+++ src/main/kotlin/net/postchain/mc/cli/util/blockchainConfig.kt
@@ -1,16 +1,12 @@
 package net.postchain.mc.cli.util
 
-import net.postchain.base.configuration.BlockchainFeatures
-import net.postchain.base.configuration.KEY_FEATURES
+import net.postchain.base.configuration.BlockchainConfigurationData
 import net.postchain.common.types.WrappedByteArray
 import net.postchain.common.wrap
 import net.postchain.gtv.Gtv
 import net.postchain.gtv.GtvEncoder
 import net.postchain.gtv.GtvFactory
 import net.postchain.gtv.gtvml.GtvMLParser
-import net.postchain.gtv.merkle.GtvMerkleHashCalculatorBase
-import net.postchain.gtv.merkle.makeMerkleHashCalculator
-import net.postchain.gtv.merkleHash
 import java.io.File
 
 class BlockchainConfig(
@@ -37,14 +33,8 @@ class BlockchainConfig(
         fun readFromByteArray(data: ByteArray): BlockchainConfig = create(GtvFactory.decodeGtv(data), data)
 
         private fun create(gtv: Gtv, data: ByteArray): BlockchainConfig {
-            val hash = gtv.merkleHash(getMerkelHashCalculator(gtv)).wrap()
-            return BlockchainConfig(hash, data, gtv)
-        }
-
-        private fun getMerkelHashCalculator(gtv: Gtv): GtvMerkleHashCalculatorBase {
-            val features = gtv[KEY_FEATURES]?.asDict()
-            val merkleHashVersion: Long = features?.get(BlockchainFeatures.merkle_hash_version.name)?.asInteger() ?: 1L
-            return makeMerkleHashCalculator(merkleHashVersion)
+            val hash = BlockchainConfigurationData.merkleHash(gtv)
+            return BlockchainConfig(hash.wrap(), data, gtv)
         }
     }
 }
