ac167c10c4f31fdcc80b7ed3b638611519cac7f1
Andrei Ursu <andrei.ursu@chromaway.com>
2025-02-17 14:19:36 +0200

    only check blockchain configurations up to --final-height

--- src/test/kotlin/net/postchain/mc/cli/blockchain/import_chain/CommandProposeFinishBlockchainImportTestIT.kt
+++ src/test/kotlin/net/postchain/mc/cli/blockchain/import_chain/CommandProposeFinishBlockchainImportTestIT.kt
@@ -87,4 +87,40 @@ class CommandProposeFinishBlockchainImportTestIT {
                 }
     }
 
+    @Test
+    fun `only check blockchain configurations up to --final-height`(@TempDir dir: Path) = RestApi(0, "").use { it ->
+        val apiUrl = "http://localhost:${it.server.port()}"
+        val dcModel = D1TestFinishImportModel(dcBcRid, apiUrl, mapOf<Long, Gtv>(0L to gtv(33L), 33L to GtvNull))
+        it.attachModel(dcBcRid, dcModel)
+        with(File(dir.toFile(), ".chromia/config")) {
+            parentFile.mkdirs()
+            writeText("""
+                api.url = $apiUrl
+                privkey = BBBDFE956021912512E14BB081B27A35A0EABC4098CB687E973C434006BCE114
+                pubkey = 03ECD350EEBC617CBBFBEF0A1B7AE553A748021FD65C7C50C5ABB4CA16D4EA5B05
+            """.trimIndent())
+        }
+        with(File(dir.toFile(), "city.configs")) {
+            parentFile.mkdirs();
+            {}.javaClass.getResourceAsStream("/city.configs")?.use { input ->
+                outputStream().use { output ->
+                    input.copyTo(output)
+                }
+            }
+        }
+
+        TestProcess.Builder("blockchain", "finish-import", "--configurations-file", "city.configs", "--final-height", "32")
+                .awaitCompletion(true)
+                .setWorkingDir(dir.toFile())
+                .start { _ ->
+                    dcModel.assertSingleOp("propose_finish_import_blockchain",
+                            listOf(
+                                    gtv("03ECD350EEBC617CBBFBEF0A1B7AE553A748021FD65C7C50C5ABB4CA16D4EA5B05".hexStringToByteArray()),
+                                    gtv("BA3C3F7984398BF2FECD85DA4E3DAE752B22CE2977004D9E4FB10ED7DA5011CD".hexStringToByteArray()),
+                                    gtv(32),
+                                    gtv("Finish blockchain import from file - blockchain-rid: BA3C3F7984398BF2FECD85DA4E3DAE752B22CE2977004D9E4FB10ED7DA5011CD, final-height: 32")
+                            ))
+                }
+    }
+
 }
