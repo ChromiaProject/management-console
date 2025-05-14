6326e1321580a4d07e1d5a18b70067820dbd2553
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-05-14 17:31:06 +0200

    Restart container

--- src/main/kotlin/net/postchain/mc/cli/container/CommandRestartContainer.kt
+++ src/main/kotlin/net/postchain/mc/cli/container/CommandRestartContainer.kt
@@ -1,11 +1,14 @@
-package net.postchain.mc.cli.container
+    package net.postchain.mc.cli.container
 
 import com.github.ajalt.clikt.core.CliktError
 import com.github.ajalt.clikt.parameters.options.required
 import net.postchain.chain0.common.queries.getContainerBlockchain
 import net.postchain.chain0.model.BlockchainState
-import net.postchain.chain0.proposal_blockchain.BlockchainAction
-import net.postchain.chain0.proposal_blockchain.proposeBlockchainActionOperation
+import net.postchain.chain0.proposal_container.ContainerAction
+import net.postchain.chain0.proposal_container.proposeContainerActionOperation
+import net.postchain.chain0.proposal_container.resumeContainerOperation
+import net.postchain.client.core.PostchainClient
+import net.postchain.client.exception.ClientError
 import net.postchain.common.BlockchainRid
 import net.postchain.mc.cli.DCBaseCommand
 import net.postchain.mc.cli.base.printResult
@@ -13,53 +16,63 @@ import net.postchain.mc.cli.util.nameOption
 
 class CommandRestartContainer : DCBaseCommand(
         name = "restart",
-        help = "Restart a container by pausing and resuming all blockchains in it.",
-        requiresVersion = 4,
+        help = "Restart a container by pausing it, waiting for it to be paused and then resuming it.",
+        requiresVersion = 94,
 ) {
     private val containerName by nameOption("Container name").required()
 
     override fun runDC() {
         val blockchains = client.getContainerBlockchain(containerName)
-                .filter { it.state == BlockchainState.RUNNING }
-                .map { BlockchainRid(it.rid) }
-
-        if (blockchains.isEmpty()) {
-            throw CliktError("No running blockchains found in container $containerName")
+        val blockchainRid = blockchains.firstOrNull { it.state == BlockchainState.RUNNING }?.let { BlockchainRid(it.rid) }
+                ?: throw CliktError("Cannot restart a container without running blockchains")
+        val blockchainClient = config.chromiaClient.getClient(blockchainRid)
+        try {
+            blockchainClient.currentBlockHeight(containerName)
+        } catch (e: ClientError) {
+            throw CliktError("Cannot restart a container without a working blockchain: ${e.message}")
         }
 
         client.transactionBuilder()
-                .apply {
-                    for (bc in blockchains) {
-                        proposeBlockchainActionOperation(
-                                clientProviderPubkey,
-                                bc,
-                                BlockchainAction.pause,
-                                "Pause blockchain $bc"
-                        )
-                    }
-                }
+                .proposeContainerActionOperation(
+                        clientProviderPubkey,
+                        containerName,
+                        ContainerAction.pause,
+                        "Pause container $containerName"
+                )
                 .postAwaitConfirmation()
                 .printResult(
-                        "Blockchains pause proposition was added successfully",
-                        "Cannot add proposal for pausing blockchains",
+                        "Container paused proposition was added successfully",
+                        "Cannot add proposal for pausing container",
                         printOnSuccess = true,
                 )
 
+        waitForContainerToPause(blockchainClient)
+
         client.transactionBuilder()
-                .apply {
-                    for (bc in blockchains) {
-                        proposeBlockchainActionOperation(
-                                clientProviderPubkey,
-                                bc,
-                                BlockchainAction.resume,
-                                "Resume blockchain $bc"
-                        )
-                    }
-                }
+                .resumeContainerOperation(
+                        clientProviderPubkey,
+                        containerName
+                )
                 .postAwaitConfirmation()
                 .printResult(
-                        "Blockchains resume proposition was added successfully",
-                        "Cannot add proposal for resuming blockchains"
+                        "Container resumed successfully",
+                        "Cannot resume container"
                 )
+    }
+
+    private fun waitForContainerToPause(blockchainClient: PostchainClient) {
+        echo("Waiting 60 seconds for container to pause...", trailingNewline = false)
+        repeat(30) {
+            try {
+                blockchainClient.currentBlockHeight(containerName)
+                echo(".", trailingNewline = false)
+            } catch (_: ClientError) {
+                echo("done")
+                return
+            }
+            Thread.sleep(2 * 1000)
+        }
+        echo("timeout!")
+        throw CliktError("Container did not pause after 60 seconds, run 'pmc container resume' to manually resume the container later.")
     }
 }
--- src/main/kotlin/net/postchain/mc/cli/container/ContainerCommand.kt
+++ src/main/kotlin/net/postchain/mc/cli/container/ContainerCommand.kt
@@ -14,5 +14,7 @@ fun containerCommands() = ContainerCommand().subcommands(
         CommandProposeContainerSubnodeImage(),
         CommandListContainers(),
         CommandProposeRemoveContainer(),
+        CommandProposePauseContainer(),
+        CommandResumeContainer(),
         CommandRestartContainer(),
 )
