282a4104a8c526aa1fa591bf99cfc4cfc616cb63
Robert Wideberg <robert.wideberg@chromaway.com>
2025-11-13 12:24:11 +0000

    Add check before approval of forced config proposal

--- src/test/kotlin/net/postchain/mc/cli/blockchain/CommandApproveForcedConfigurationIT.kt
+++ src/test/kotlin/net/postchain/mc/cli/blockchain/CommandApproveForcedConfigurationIT.kt
@@ -3,7 +3,12 @@ package net.postchain.mc.cli.blockchain
 import assertk.assertThat
 import assertk.assertions.isTrue
 import net.postchain.chain0.proposal_blockchain.APPROVE_PROPOSED_FORCED_CONFIGURATION
-import net.postchain.common.BlockchainRid
+import net.postchain.chain0.proposal_blockchain.ForcedConfigurationProposalData
+import net.postchain.common.wrap
+import net.postchain.gtv.GtvEncoder
+import net.postchain.gtv.GtvFactory.gtv
+import net.postchain.gtv.mapper.GtvObjectMapper
+import net.postchain.mc.cli.test_helpers.DEFAULT_BRID_ECONOMY_CHAIN
 import net.postchain.mc.cli.test_helpers.ManagedRestTestApi
 import net.postchain.mc.cli.test_helpers.assertCommandSuccessContains
 import org.junit.jupiter.api.Test
@@ -12,18 +17,24 @@ import java.nio.file.Path
 
 class CommandApproveForcedConfigurationIT {
 
-    private val blockchainRID = BlockchainRid.buildRepeat(10)
-
     @Test
     fun `approve forced configuration`(@TempDir dir: Path) {
         ManagedRestTestApi(dir, dcVersion = 85)
+                .withDCQuery("get_forced_configuration_stage2_proposal", GtvObjectMapper.toGtvDictionary(ForcedConfigurationProposalData(
+                        blockchainRid = DEFAULT_BRID_ECONOMY_CHAIN.wData,
+                        currentHeight = 567,
+                        currentConfiguration = GtvEncoder.encodeGtv(gtv(mapOf("foo" to gtv(17)))).wrap(),
+                        proposedHeight = 567,
+                        proposedConfiguration = GtvEncoder.encodeGtv(gtv(mapOf("foo" to gtv(23)))).wrap(),
+                )))
+                .withECModel { it.height = 567 }
                 .testCommand(
                         CommandApproveForcedConfiguration(),
-                        "-brid", blockchainRID.toHex(),
+                        "-brid", DEFAULT_BRID_ECONOMY_CHAIN.toHex(),
                 ) { result, api ->
                     assertCommandSuccessContains(result, "Forced configurations was approved")
                     assertThat(api.getDcModel().opWasCalled(APPROVE_PROPOSED_FORCED_CONFIGURATION) {
-                        it[1].asByteArray().contentEquals(blockchainRID.data)
+                        it[1].asByteArray().contentEquals(DEFAULT_BRID_ECONOMY_CHAIN.data)
                     }).isTrue()
                 }
     }
