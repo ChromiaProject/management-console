1220e6e2caf19105ce91d8859a164682772aeb3d
Eugene Tykulov <eugene.tykulov@chromaway.com>
2026-06-25 10:22:14 +0000

    Fail check-cluster-removal-status for unknown clusters

--- src/main/kotlin/net/postchain/mc/cli/node/CommandCheckClusterRemovalStatus.kt
+++ src/main/kotlin/net/postchain/mc/cli/node/CommandCheckClusterRemovalStatus.kt
@@ -1,15 +1,19 @@
 package net.postchain.mc.cli.node
 
+import com.github.ajalt.clikt.core.CliktError
 import com.github.ajalt.clikt.core.terminal
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import net.postchain.anchoring.anchoring_chain_common.getLastAnchoredBlock
 import net.postchain.chain0.cm_api.cmGetClusterInfo
 import net.postchain.chain0.cm_api.cmGetSystemAnchoringChain
+import net.postchain.chain0.common.queries.getClusters
 import net.postchain.chain0.common.queries.getNodeSignerClusterBlockchains
+import net.postchain.chain0.common.queries.getNodesWithProvider
 import net.postchain.chain0.model.BlockchainState
 import net.postchain.chain0.model.ContainerState
 import net.postchain.common.BlockchainRid
+import net.postchain.common.types.WrappedByteArray
 import net.postchain.common.wrap
 import net.postchain.mc.cli.DCBaseCommand
 import net.postchain.mc.cli.util.pmcTable
@@ -27,6 +31,12 @@ class CommandCheckClusterRemovalStatus : DCBaseCommand(
     private val cluster by option("-c", "--cluster").required()
 
     override fun runDC() {
+        if (config.client.getClusters().none { it.name == cluster }) {
+            throw CliktError("Cluster '$cluster' does not exist")
+        }
+        if (config.client.getNodesWithProvider().none { it.pubkey == WrappedByteArray(key.data) }) {
+            throw CliktError("Node with pubkey '${key.hex()}' does not exist")
+        }
         if (terminal.terminalInfo.outputInteractive) echo("Fetching chains that still has node as signer in latest confirmed configuration...")
         val remainingSignerChains = config.client.getNodeSignerClusterBlockchains(
                 key,
